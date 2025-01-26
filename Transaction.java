a372a23312b2000f78d31e81211779824c325883
Sanchay Sinha <sinhasanchay0208@gmail.com>
2025-01-26 20:49:27 +0530

    Add files via upload

---
+++ Transaction.java
@@ -0,0 +1,23 @@
+class Transaction {
+    private String description;
+    private double amount;
+    private String category;
+
+    public Transaction(String description, double amount, String category) {
+        this.description = description;
+        this.amount = amount;
+        this.category = category;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+}
